fe2bf4f639e955064d909c1fcdffcead329a6c7e
Gokul Madhusudhan <gokul99.gm@gmail.com>
2020-12-12 14:12:07 +0530

    images uploaded to ipfs

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Artistry = artifacts.require("Artistry");
+
+module.exports = function (deployer) {
+  deployer.deploy(Artistry);
+};
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+import photo from '../photo.png'
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+
+        <div style={{ color: "white" }} className="ml-5">Artistry</div>
+
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{this.props.account}</small>
+            </small>
+            {this.props.account
+              ? <img
+                className='ml-2'
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+              />
+              : <span></span>
+            }
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
